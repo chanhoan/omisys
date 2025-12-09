f99b56ed87f66f760c772e458e46ed410c18a49d
정찬환 <chanhoan01@gmail.com>
2025-12-10 03:20:48 +0900

    유레카 서비스 구축

--- service/eureka/server/src/main/java/com/omisys/eureka/ServerApplication.java
+++ service/eureka/server/src/main/java/com/omisys/eureka/ServerApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+@SpringBootApplication
+public class ServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
 
 }
