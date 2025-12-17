0348df6c5f5d15d1d1606fc3bb36621de008877c
정찬환 <chanhoan01@gmail.com>
2025-12-17 21:03:26 +0900

    elasticsearch ssl

--- service/product/server/src/main/java/com/omisys/product/ProductApplication.java
+++ service/product/server/src/main/java/com/omisys/product/ProductApplication.java
@@ -2,10 +2,15 @@ package com.omisys.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@SpringBootApplication(exclude = {
+        ElasticsearchRestClientAutoConfiguration.class,
+        ElasticsearchDataAutoConfiguration.class
+})
 @EnableAsync
-@SpringBootApplication
 public class ProductApplication {
 
     public static void main(String[] args) {
