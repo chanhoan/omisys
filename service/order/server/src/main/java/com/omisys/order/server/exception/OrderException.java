a36b85bd3817c9b59fe11eb2cad8fc82bd310bdb
정찬환 <chanhoan01@gmail.com>
2026-04-05 16:57:01 +0900

    refactor: use lombok @Getter annotation against method

--- service/order/server/src/main/java/com/omisys/order/server/exception/OrderException.java
+++ service/order/server/src/main/java/com/omisys/order/server/exception/OrderException.java
@@ -1,18 +1,16 @@
 package com.omisys.order.server.exception;
 
 import com.omisys.common.domain.exception.BusinessException;
+import lombok.Getter;
 
 public class OrderException extends BusinessException {
 
+    @Getter
     private final OrderErrorCode errorCode;
 
     public OrderException(OrderErrorCode errorCode, Object... args) {
         super(errorCode.getStatus().name(), errorCode.getMessage(), args);
         this.errorCode = errorCode;
-    }
-
-    public OrderErrorCode getErrorCode() {
-        return errorCode;
     }
 
 }
